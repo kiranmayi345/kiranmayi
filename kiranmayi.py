dc1f76cff11e6b689a7f9b89ee55aed255fe2bab
kiranmayi345 <155666300+kiranmayi345@users.noreply.github.com>
2024-01-12 10:26:05 +0530

    Create kiranmayi.py

---
+++ kiranmayi.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.header('power star')
+st.write('pawan kalyan')
+st.image('https://www.deccanchronicle.com/_next/image?url=http%3A%2F%2Fimages.deccanchronicle.com%2Fdc-Cover-r16bq4lfodq201vn0e13vs8em4-20161101001949.Medi.jpeg&w=1920&q=75',caption='pawankalyan')
